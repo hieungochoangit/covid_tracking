9e3841533b50b02e9bb6ae4e9ada1f715294c9fa
hieuhn <hieungochoangit@gmail.com>
2022-02-23 00:32:24 +0700

    Api get report country

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,12 @@
 import axios from "axios";
+import moment from "moment";
 
 export function getCountries() {
   return axios.get('https://api.covid19api.com/countries');
+}
+
+export function getReportCountry(slug) {
+  return axios.get(`https://api.covid19api.com/dayone/country/${slug}?from=2021-01-01T00:00:00&to=${moment()
+  .utc(0)
+  .format()}`);
 }

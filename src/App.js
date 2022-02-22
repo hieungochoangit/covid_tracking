dece62fe5e6008e16b06556177c5eb177bd0b192
hieuhn <hieungochoangit@gmail.com>
2022-02-22 20:37:44 +0700

    Get countries from api and display ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import { FormControl, InputLabel, MenuItem, NativeSelect, Select, Typography } from '@mui/material';
+
+import { getCountries } from './api/api';
+import { Box } from '@mui/system';
+
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries().then(res => {
+      setCountries(res.data);
+      console.log(res.data)
+    })
+  }, [])
+
+  const handleSelected = () => {
+    console.log("checked")
+  }
+
   return (
     <div className="App">
-      
+      <Typography variant='h1' fontWeight='500'>So lieu COVID-19</Typography>
+      <Box width='300px'>
+        <FormControl fullWidth>
+          <NativeSelect onChange={handleSelected}>
+            {
+              countries && countries.map((item, index) => <option key={index} value={item.ISO2}>{item.Country}</option>)
+            }
+          </NativeSelect>
+        </FormControl>
+      </Box>
     </div>
   );
 }
